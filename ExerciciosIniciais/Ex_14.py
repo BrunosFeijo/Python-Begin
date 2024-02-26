13e3cbd3b91312d794f8f828312c1a0a012bbaa5
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-02-26 16:57:47 -0300

    done-ex_14

---
+++ ExerciciosIniciais/Ex_14.py
@@ -0,0 +1,6 @@
+# 14. Lê um valor de hora e informa quantos minutos se passaram desde o início do dia
+
+horas = int(input('Digite o número de horas: '))
+minutos = horas * 60
+
+print(f'Em {horas}h temos {minutos} minutos')
