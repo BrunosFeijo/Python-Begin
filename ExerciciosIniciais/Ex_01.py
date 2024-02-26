2564bb7d92d543e9da7aa290c85ed145393ad512
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-02-26 16:01:06 -0300

    done-ex_01

---
+++ ExerciciosIniciais/Ex_01.py
@@ -0,0 +1,6 @@
+# 1. Mostrar na tela o produto entre dois números fornecidos pelo usuário
+number1 = int(input('Digite um número: '))
+number2 = int(input('Digite outro número: '))
+product = number1 * number2
+
+print('A soma dos números digitados é:', product)
