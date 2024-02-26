a846f5daa3f4a81dc46c3f3c5f49fde9ca056b42
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-02-26 16:15:45 -0300

    done-ex_04

---
+++ ExerciciosIniciais/Ex_04.py
@@ -0,0 +1,7 @@
+#4. Lê dois números e mostra a soma. Ante do resultado, deverá aparecer a mensagem: SOMA.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+
+print(f'A soma é {soma}')
