f22bef5cd7f95976d89e24564a424310a969a05b
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-02-27 20:43:51 -0300

    done-senquential-ex07

---
+++ ExerciciosSequenciais/Ex07.py
@@ -0,0 +1,5 @@
+# Questão 7. Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro
+# desta área para o usuário.
+lado = float(input('Digite o lado de um quadrado: '))
+area = lado * lado
+print(f'A área do quadrado de lado {lado} é {area} e o seu dobro é igual a {area * 2}')
