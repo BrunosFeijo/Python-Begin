2be44082be1f85414eac05b2de7a3adb631f2610
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-02-27 20:54:00 -0300

    done-senquential-ex09

---
+++ ExerciciosSequenciais/Ex09.py
@@ -0,0 +1,5 @@
+# Questão 9. Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e
+# mostre a temperatura em graus Celsius. C = (5 * (F-32) / 9).
+fahrenheit = float(input('Digite a temperatura em fahrenheit: '))
+celsius = (5 * (fahrenheit-32) / 9)
+print(f'A temperatura em celsius é: {celsius:.2f}°', )
