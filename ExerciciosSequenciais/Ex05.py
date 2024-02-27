6d22797afb31676f043121ae3c97b0c66b262fef
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-02-27 20:32:17 -0300

    done-senquential-ex05

---
+++ ExerciciosSequenciais/Ex05.py
@@ -0,0 +1,4 @@
+# Questão 5. Faça um Programa que converta metros para centímetros.
+metros = float(input('Digite uma medida em metros: '))
+centimetros = metros * 100
+print(f'A medida de {metros} metros em centímetros é {centimetros:.0f}')
