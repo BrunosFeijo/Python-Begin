2d163db1224766b3cc73d6fc0bd5b2d7ec7faadf
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-03-03 15:09:56 -0300

    done-senquential-ex28

---
+++ ExerciciosSequenciais/Ex28.py
@@ -0,0 +1,6 @@
+# Questão 28. Lê um número e mostra seu sucessor e seu antecessor na tela.
+
+n = int(input('Digite um número: '))
+print(f'Número:{n}')
+print(f'Antecessor: {n - 1}')
+print(f'Sucessor: {n + 1}')
