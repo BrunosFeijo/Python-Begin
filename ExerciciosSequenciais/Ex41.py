89f6a8877210a95ad5e589d9d5f855fa29ef99bc
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-03-03 18:49:07 -0300

    done-senquential-ex41

---
+++ ExerciciosSequenciais/Ex41.py
@@ -0,0 +1,7 @@
+# Questão 41. Elabora um algoritmo onde o usuário informa a base e a altura de um retângulo e ao
+# final imprime a área
+
+base = float(input('Digite o valor da base: '))
+altura = float(input('Digite o valor da altura: '))
+area = base * altura
+print(f'Area {base}x{altura} = {area}')
