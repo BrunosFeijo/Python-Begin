f15566740cd5d2e8ae3b1e71c3621ecad19ef0cb
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-03-03 18:45:57 -0300

    done-senquential-ex40

---
+++ ExerciciosSequenciais/Ex40.py
@@ -0,0 +1,5 @@
+# Questão 40. Faça um algoritmo para calcular quantas ferraduras são necessárias para equipar
+# todos os cavalos comprados para um haras
+qtdCavalos = int(input('Quantos cavalos foram comprados: '))
+qtdFerraduras = qtdCavalos * 4
+print(f'Qtd ferraduras: {qtdFerraduras}')
