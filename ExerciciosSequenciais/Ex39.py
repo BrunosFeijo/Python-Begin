cb92045f981f872ae8b8fbe72988e77768b45f2b
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-03-03 18:42:49 -0300

    done-senquential-ex39

---
+++ ExerciciosSequenciais/Ex39.py
@@ -0,0 +1,6 @@
+# Questão 39. Elabore um algoritmo que lê um número e imprime a raiz quadrada do número.
+import math
+
+valor = float(input('Digite o valor: '))
+raiz = math.sqrt(valor)
+print(f'A raiz é : {raiz:.2f}')
