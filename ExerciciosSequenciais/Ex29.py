9d27eb184918b31dbfa5e6bf056a9595cd7e4eac
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-03-03 15:11:39 -0300

    done-senquential-ex29

---
+++ ExerciciosSequenciais/Ex29.py
@@ -0,0 +1,7 @@
+# Questão 29. Lê dois números e mostra soma. Antes do resultado, deverá aparecer a mensagem:
+# SOMA.
+numeros= [
+    float(input('Digite o 1° número: ')),
+    float(input('Digite o 2° número: '))
+]
+print(f'A soma dos números é: {sum(numeros)}')
