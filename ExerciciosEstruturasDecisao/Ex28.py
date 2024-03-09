548209cba894a6fd00bfef3b98395da97d1e4c81
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-03-09 12:54:55 -0300

    done-loop_structure-Ex28

---
+++ ExerciciosEstruturasDecisao/Ex28.py
@@ -0,0 +1,17 @@
+# Questão 28. Faça um programa que mostra os números múltiplos de 5 ou múltiplos de 6, no
+# intervalo de 100 a 300.
+
+def criaListaMultiplos(start, stop, numeroBase):
+    listaMultiplos = [
+        numero
+        for numero in range(start, stop + 1)
+        if numero % numeroBase == 0
+    ]
+    return listaMultiplos
+
+start = 100
+stop = 300
+listaMultiplosCinco = criaListaMultiplos(start, stop, 5)
+listaMultiplosSeis = criaListaMultiplos(start,stop,6)
+print('Multiplos 5:',listaMultiplosCinco)
+print('Multiplos 6:', listaMultiplosSeis)
