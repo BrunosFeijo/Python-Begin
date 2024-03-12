2e555088225f66ae4a02e4d76315080655ef3574
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-03-11 22:11:40 -0300

    create-class

---
+++ Classes/carro.py
@@ -0,0 +1,26 @@
+class Carro():
+    marca = ''
+    modelo = ''
+    ano = 0
+    placa = ''
+    chassi = ''
+    cor = ''
+    cor_lateral = ''
+    velocidade = 0.0
+    consumo = 0.0
+    tipo_combustivel = ''
+
+    def __init__(self):
+        ...
+
+    def movimentar():
+        ...
+
+    def acelerar():
+        ...
+
+    def estacionar():
+        ...
+
+    def get_velocidade(self):
+        return self.velocidade
---
+++ Classes/main.py
@@ -0,0 +1,16 @@
+from carro import Carro
+
+fusca = Carro()
+fusca.marca = 'Volkswagen'
+fusca.modelo = 'Fusca'
+fusca.ano = 1965
+
+print(f'Carro {fusca.marca} - {fusca.modelo} - {fusca.ano}')
+
+cheva = Carro()
+cheva.ano = 1965
+cheva.marca = 'Chevrolet'
+cheva.modelo = 'Chevrolet Tubarão'
+
+print(f'Carro {cheva.marca} - {cheva.modelo} - {cheva.ano}')
+
