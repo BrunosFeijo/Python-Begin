58931bbfd35bea2ca9f91c91b7bee4184a2fae74
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-03-17 17:03:17 -0300

    fixed-class-livro

--- ExerciciosClasses/livro.py
+++ ExerciciosClasses/livro.py
@@ -3,13 +3,14 @@ from datetime import datetime
 
 class Livro:
     def __init__(self, cod: str, titulo: str, resumo: str, isbn: str, publicacao: datetime):
-        if not isinstance(cod, str) and len(cod) == 10:
-            raise TypeError('Um código deve ser uma string de 10 caracteres')
-        if not isinstance(titulo, str) and 1 <= len(titulo) <= 100:
+        if not isinstance(cod, str) or not len(cod) == 10:
+            print(len(cod), type(cod))
+            raise TypeError(f'Um código deve ser uma string de 10 caracteres.')
+        if not isinstance(titulo, str) or not 1 <= len(titulo) <= 100:
             raise TypeError('Um código deve ser uma string de 1 a 100 caracteres')
-        if not isinstance(resumo, str) and 10 <= len(resumo) <= 500:
+        if not isinstance(resumo, str) or not 10 <= len(resumo) <= 500:
             raise TypeError('Uma resumo deve ser uma string de 10 a 500 caracteres')
-        if not isinstance(isbn, str) and len(isbn) == 13:
+        if not isinstance(isbn, str) or not len(isbn) == 13:
             raise TypeError('Um isbn deve ser uma string de 13 caracteres')
         if not isinstance(publicacao, datetime):
             raise TypeError('Um publicacao deve ser um datetime')
@@ -24,3 +25,34 @@ class Livro:
         retorno = f'Código: {self.cod} \nTítulo: {self.titulo} \nResumo: {self.resumo} \nIsbn: {self.isbn} \nPublicacao: {self.publicacao}'
 
         return retorno
+
+    def get_cod(self):
+        return self.cod
+
+    def set_cod(self, cod):
+        self.cod = cod
+
+    def get_titulo(self):
+        return self.titulo
+
+    def set_titulo(self, titulo):
+        self.titulo = titulo
+
+    def get_resumo(self):
+        return self.resumo
+
+    def set_resumo(self, resumo):
+        self.resumo = resumo
+
+    def get_isbn(self):
+        return self.isbn
+
+    def set_isbn(self, isbn):
+        self.isbn = isbn
+
+    def get_publicacao(self):
+        return self.publicacao
+
+    def set_publicacao(self, publicacao):
+        self.publicacao = publicacao
+
--- ExerciciosClasses/main.py
+++ ExerciciosClasses/main.py
@@ -1,9 +1,14 @@
 import datetime as dt
 
+from ExerciciosClasses.livro import Livro
 from autor import Autor
 
 autor = Autor("Bruno", dt.datetime(1994, 1, 27), "OutroNome")
+livro = Livro("1234567890","Livro Teste", "Este é um livro teste.", "1234567891234",dt.datetime.now())
+
 
 print('-' * 40)
 print(autor)
+print('-' * 40)
+print(livro)
 print('-' * 40)
