bf956e89d2d785b99c6f97f5288d872007adcbfa
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-03-17 13:13:08 -0300

    done-functions-Ex06

--- ExerciciosFuncao/Ex06.py
+++ ExerciciosFuncao/Ex06.py
@@ -1,1 +1,14 @@
-# 
+# Questão 6. Faça uma função que recebe a idade de uma pessoa em anos, meses e dias e retorna
+# essa idade expressa em dias.
+
+# assumindo anos de 365 dias e meses de 30 dias
+def calcularDias(anos, meses, dias):
+    return anos * 365 + meses * 30 + dias
+
+
+print('Digite sua idade em anos, meses e dias: ')
+ano = int(input('Digite o ano: '))
+mes = int(input('Digite o mês: '))
+dia = int(input('Digite o dia: '))
+
+print("Sua idade em dias é: ", calcularDias(ano, mes, dia))
